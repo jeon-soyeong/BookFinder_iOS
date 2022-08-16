0db5998fc80c90f8c0316c86f8887eb3c9d31705
전소영 <jsu3417@gmail.com>
2022-08-17 00:08:56 +0900

    BookList model 추가

---
+++ BookFinder/BookFinder/Sources/BookList/Model/BookList.swift
@@ -0,0 +1,37 @@
+//
+//  BookList.swift
+//  BookFinder
+//
+//  Created by 전소영 on 2022/08/16.
+//
+
+import Foundation
+
+// MARK: - BookList
+struct BookList: Codable {
+    let totalItems: Int
+    let items: [BookItem]
+}
+
+// MARK: - Item
+struct BookItem: Codable {
+    let id: String
+    let volumeInfo: VolumeInfo
+    let etag: String
+}
+
+// MARK: - VolumeInfo
+struct VolumeInfo: Codable {
+    let title: String
+    let subtitle: String?
+    let authors: [String]?
+    let publishedDate: String?
+    let imageLinks: ImageLinks?
+    let infoLink: String
+    let description: String?
+}
+
+// MARK: - ImageLinks
+struct ImageLinks: Codable {
+    let thumbnail: String
+}
